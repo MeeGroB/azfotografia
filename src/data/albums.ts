b68d4b47e89ec7465453f69d3726ad3094928ad3
Pier Chavez <pier.cmont@gmail.com>
2025-12-03 11:42:00 +0100

    add albums data

--- src/data/albums.ts
+++ src/data/albums.ts
@@ -7,6 +7,12 @@ import fotoMC5 from "../assets/albumsData/MariaCarlos/foto5.webp";
 import fotoMC6 from "../assets/albumsData/MariaCarlos/foto6.webp";
 import fotoMC7 from "../assets/albumsData/MariaCarlos/foto6.webp";
 
+import fotoAR1 from "../assets/albumsData/AnaRoberto/foto1.webp";
+import fotoAR2 from "../assets/albumsData/AnaRoberto/foto2.webp";
+import fotoAR3 from "../assets/albumsData/AnaRoberto/foto3.webp";
+import fotoAR4 from "../assets/albumsData/AnaRoberto/foto4.webp";
+import fotoAR5 from "../assets/albumsData/AnaRoberto/foto5.webp";
+import fotoAR6 from "../assets/albumsData/AnaRoberto/foto6.webp";
 
 export const albumsData = [
     {
@@ -26,5 +32,25 @@ export const albumsData = [
             fotoMC6,
             fotoMC7
         ]
+    },
+
+    {
+        id:2,
+        coupleName: "Ana & Roberto",
+        location: "Huacachina, Ica",
+        date: "25 de Agosto, 2025",
+        coverImage: fotoAR5,
+        photoCount: 85,
+        description: "Una ceremonia íntima frente al oasis de la Huacachina con una puesta de sol inolvidable",
+        photos: [
+            fotoAR1,
+            fotoAR2,
+            fotoAR3,
+            fotoAR4,
+            fotoAR5,
+            fotoAR6
+        ]
     }
+
+
 ]
