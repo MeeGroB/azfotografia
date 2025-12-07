2503a1c507f40bb7404bc16329c3314f119c3df5
Pier Chavez <pier.cmont@gmail.com>
2025-12-07 10:50:52 +0100

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,32 +2,33 @@
 import { FaInstagram, FaYoutube, FaFacebook, FaTiktok   } from "react-icons/fa";
 
 import logoImg from '../assets/logo.webp';
+import { Mail, Phone } from "lucide-react";
 
 const Footer = () => {
 
     // const location = useLocation();
 
-    // const scrollToSection = (sectionId: string) => {
-    //     if(location.pathname !== '/') {
-    //         window.REACT_APP_NAVIGATE('/');
-    //         setTimeout(() => {
-    //             const element = document.getElementById(sectionId);
+    const scrollToSection = (sectionId: string) => {
+        if(location.pathname !== '/') {
+            window.REACT_APP_NAVIGATE('/');
+            setTimeout(() => {
+                const element = document.getElementById(sectionId);
 
-    //             if(element) {
-    //                 element.scrollIntoView({ behavior: 'smooth'})
-    //             }
-    //         }, 1000);
-    //     } else {
-    //         const element = document.getElementById(sectionId);
-    //         if(element) {
-    //             element.scrollIntoView({ behavior: 'smooth'})
-    //         }
-    //     }
-    // }
+                if(element) {
+                    element.scrollIntoView({ behavior: 'smooth'})
+                }
+            }, 1000);
+        } else {
+            const element = document.getElementById(sectionId);
+            if(element) {
+                element.scrollIntoView({ behavior: 'smooth'})
+            }
+        }
+    }
 
-    // const navigateToPage = (path: string)=> {
-    //     window.REACT_APP_NAVIGATE(path)
-    // }
+    const navigateToPage = (path: string)=> {
+        window.REACT_APP_NAVIGATE(path)
+    }
 
   return (
     <footer className="bg-linear-to-br from-gray-900 via-gray-800 to bg-gray-900 text-white">
@@ -80,7 +81,104 @@ const Footer = () => {
                         </a>
                     </div>
                 </div>
+
+
+                {/* Enlaces Rapidos */}
+                <div>
+                    <h3 className="text-lg font-semibold mb-4 text-primary">
+                        Enlaces Rápidos
+                    </h3>
+
+                    <ul className="space-y-3">
+                        <li>
+                            <button
+                                onClick={()=> scrollToSection('inicio')}
+                                className="text-gray-300 hover:text-primary transition-colors cursor-pointer whitespace-nowrap"
+                            >
+                                Inicio
+                            </button>
+                        </li>
+
+                        <li>
+                            <button
+                                onClick={()=> scrollToSection('about')}
+                                className="text-gray-300 hover:text-primary transition-colors cursor-pointer whitespace-nowrap"
+                            >
+                                Sobre Mi
+                            </button>
+                        </li>
+
+                        <li>
+                            <button
+                                onClick={()=> scrollToSection('services')}
+                                className="text-gray-300 hover:text-primary transition-colors cursor-pointer whitespace-nowrap"
+                            >
+                                Servicios
+                            </button>
+                        </li>
+
+                        <li>
+                            <button
+                                onClick={()=> navigateToPage('/precios')}
+                                className="text-gray-300 hover:text-primary transition-colors cursor-pointer whitespace-nowrap"
+                            >
+                                Precios
+                            </button>
+                        </li>
+
+                        <li>
+                            <button
+                                onClick={()=> scrollToSection('gallery')}
+                                className="text-gray-300 hover:text-primary transition-colors cursor-pointer whitespace-nowrap"
+                            >
+                                Galería
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+
+                {/* Contacto */}
+                <div>
+                    <h3 className="text-lg font-semibold mb-4 text-primary">
+                        Contacto
+                    </h3>
+                    <ul className="space-y-3">
+                        <li className="flex items-start gap-2">
+                            <Phone className="text-primary h-5 w-5" />
+                            <div>
+                                <p className="text-gray-300">
+                                    +51 997 831 816
+                                </p>
+                            </div>
+                        </li>
+
+                        <li className="flex items-start gap-2 wrap-anywhere">
+                            <Mail className="text-primary h-5 w-5" />
+                            <div>
+                                <p className="text-gray-300">
+                                    contacto@azfotografia.com
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+
+                </div>
             </div>
+
+            {/* Copyright */}
+            <div className="border-t border-gray-700 mt-12 pt-8 text-center">
+                <p className="text-gray-400">
+                    © {new Date().getFullYear()} AZ - Fotografía - Alejandro. Todos los derechos reservados
+                </p>
+
+                <p className="text-gray-400 text-base mt-2">
+                    Hecho por {""}
+                    <a href="#" className="text-gray-300 hover:text-primary transition-colors duration-300">Pier Chávez Monteblanco</a>
+                </p>
+            </div>
+
+
+
         </div>
     </footer>
   )
